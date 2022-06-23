7234d2c24c5e7ae33f12d8376c918181bb1b9d75
Antonia Luciana Pires <antonialucianapires@gmail.com>
2022-06-23 00:18:25 -0300

    feat: definição de tamanho parametrizável do avatar

--- src/components/avatar/Avatar.tsx
+++ src/components/avatar/Avatar.tsx
@@ -1,7 +1,7 @@
 import styles from './Avatar.module.css';
 
-export function Avatar() {
+export function Avatar({isMedium = false}) {
     return (
-        <img className={styles.avatar} src="https://images.unsplash.com/photo-1530785602389-07594beb8b73?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8d29tYW4lMjBibGFja3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60" />
+        <img className={isMedium ? styles.avatarMedium : styles.avatarSmall} src="https://images.unsplash.com/photo-1530785602389-07594beb8b73?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8d29tYW4lMjBibGFja3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60" />
     );
 }
