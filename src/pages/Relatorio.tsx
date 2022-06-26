f2b2f8cecb0b07e3a023ffd61edc21bc1a9cd6c3
Antonia Luciana Pires <antonialucianapires@gmail.com>
2022-06-25 23:54:39 -0300

    feat: adição do relatório estático na tela de relatórios conforme protótipo

--- src/pages/Relatorio.tsx
+++ src/pages/Relatorio.tsx
@@ -1,9 +1,30 @@
+import { CardInfoDash } from "../components/card/infodash/CardInfoDash";
 import { Header } from "../components/header/Header";
+import { useFetch } from "../hooks/useFetch";
+import styles from './Relatorio.module.css';
+
+type infosCard = {
+    title: string;
+    value: string;
+}
 
 export function Relatorio() {
+
+    const { data: repositories } = useFetch<infosCard[]>('http://localhost:3100/api/sav/v1/resume');
+
     return (
-        <div>
+        <div className={styles.relatorio}>
             <Header title="Relatórios" username="Andreia Gomes" />
+            <section className={styles.listCardInfo}>
+                {
+                    repositories?.map(item => {
+                        return (
+                            <CardInfoDash title={item.title} value={item.value} />
+                        )
+                    })
+                }
+
+            </section>
         </div>
     );
 }
