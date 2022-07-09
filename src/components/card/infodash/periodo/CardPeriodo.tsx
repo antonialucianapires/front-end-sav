5e97ba604d00df0a25d3a95dd0aa3a87d9d5785e
Antonia Luciana Pires <antonialucianapires@gmail.com>
2022-07-09 17:55:33 -0300

    fix: ajuste link opcoes periodo

--- src/components/card/infodash/periodo/CardPeriodo.tsx
+++ src/components/card/infodash/periodo/CardPeriodo.tsx
@@ -45,7 +45,7 @@ export function CardPeriodo(periodo: Periodo) {
                 <DotsThreeOutlineVertical size={25} color={"var(--gray-500)"} onClick={mostrarOpcoesFuncao} cursor={"pointer"} />
             </div>
             <ul id='listaOpcoes' className={mostrarOpcoes ? styles.comOpcoes : styles.semOpcoes}>
-                <li><Link to={"/periodos/" + periodo.idPeriodo}>Visualizar</Link></li>
+                <li><Link className={styles.opcaoHabilitada} to={"/periodos/" + periodo.idPeriodo}>Visualizar</Link></li>
                 <li className={isDesabilidado ? styles.opcaoDesabilitada : styles.opcaoHabilitada}>Editar</li>
                 <li className={isDesabilidado ? styles.opcaoDesabilitada : styles.opcaoHabilitada}>Excluir</li>
             </ul>
