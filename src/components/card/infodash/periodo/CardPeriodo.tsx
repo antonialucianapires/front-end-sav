25a96e90242deeddc35a98bf4e5582e25f6c2a3e
Antonia Luciana Pires <antonialucianapires@gmail.com>
2022-07-08 22:59:21 -0300

    feat: utilização do useState para mostrar e ocultar opções de um card período

--- src/components/card/infodash/periodo/CardPeriodo.tsx
+++ src/components/card/infodash/periodo/CardPeriodo.tsx
@@ -1,3 +1,5 @@
+import { DotsThreeOutlineVertical } from 'phosphor-react';
+import { useState } from 'react';
 import styles from './CardPeriodo.module.css';
 
 type Periodo = {
@@ -21,10 +23,28 @@ export function CardPeriodo(periodo: Periodo) {
             break;
     }
 
+    const [mostrarOpcoes, setMostrarOpcoes] = useState(false);
+
+    function mostrarOpcoesFuncao() {
+        if (mostrarOpcoes) {
+            setMostrarOpcoes(false)
+        } else {
+            setMostrarOpcoes(true)
+        }
+    }
+
     return (
         <div className={styles.cardPeriodo}>
             <h3>{periodo.nome}</h3>
-            <p className={style}>{periodo.status}</p>
+            <div className={styles.statusBox}>
+                <p className={style}>{periodo.status}</p>
+                <DotsThreeOutlineVertical size={25} color={"var(--gray-500)"} onClick={mostrarOpcoesFuncao} cursor={"pointer"} />
+            </div>
+            <ul id='listaOpcoes' className={mostrarOpcoes ? styles.comOpcoes : styles.semOpcoes}>
+                <li>Visualizar</li>
+                <li>Editar</li>
+                <li>Excluir</li>
+            </ul>
         </div>
     );
 }
